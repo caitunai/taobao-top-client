21c2791f62d527bd509f398e11581fa243318652
iflamed <iflamed@gmail.com>
2018-08-30 12:12:07 +0800

    add taobaoke request and domain

---
+++ src/Top/Domain/MapData.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Top\Domain;
+
+/**
+ * data.
+ *
+ * @author auto create
+ */
+class MapData
+{
+    /**
+     * password.
+     **/
+    public $model;
+}
---
+++ src/Top/Domain/Trackparams.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Top\Domain;
+
+/**
+ * 埋点信息.
+ *
+ * @author auto create
+ */
+class Trackparams
+{
+    /**
+     * empty.
+     **/
+    public $empty;
+}
